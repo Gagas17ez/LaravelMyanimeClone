ffa774381d597908e649c698735760abeb24b6c0
kendahana <izwar.jati@gmail.com>
2021-08-05 15:09:24 +0700

    commit ui

---
+++ resources/views/master-parts/scripts.blade.php
@@ -0,0 +1,9 @@
+<!-- Js Plugins -->
+<script src="{{asset('template.anime/js/jquery-3.3.1.min.js')}}"></script>
+<script src="{{asset('template.anime/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('template.anime/js/player.js')}}"></script>
+<script src="{{asset('template.anime/js/jquery.nice-select.min.js')}}"></script>
+<script src="{{asset('template.anime/js/mixitup.min.js')}}"></script>
+<script src="{{asset('template.anime/js/jquery.slicknav.js')}}"></script>
+<script src="{{asset('template.anime/js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('template.anime/js/main.js')}}"></script>
