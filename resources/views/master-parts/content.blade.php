22f7c81f427b7ee3776e8b506f8b85221fdd519c
kendahana <izwar.jati@gmail.com>
2021-08-05 16:13:52 +0700

    pemindahan berbagai element agar lebih mudah dibaca

---
+++ resources/views/master-parts/content.blade.php
@@ -0,0 +1,6 @@
+        <!-- Hero Section Begin -->
+            @yield('hero')
+        <!-- Hero Section End -->
+        <!-- Main Section Begin -->
+            @yield('main')
+        <!-- Main Section End -->
