25f8709281c60dee107889e9d66dfa7e5b0b4910
kendahana <izwar.jati@gmail.com>
2021-08-05 16:10:27 +0700

    pemindahan berbagai element agar lebih mudah dibaca

--- resources/views/master-parts/scripts.blade.php
+++ resources/views/master-parts/scripts.blade.php
@@ -1,4 +1,4 @@
-<!-- Js Plugins -->
+
 <script src="{{asset('template.anime/js/jquery-3.3.1.min.js')}}"></script>
 <script src="{{asset('template.anime/js/bootstrap.min.js')}}"></script>
 <script src="{{asset('template.anime/js/player.js')}}"></script>
